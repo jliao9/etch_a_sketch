0b0ff557820ded4060601aabe2e0770b4e6f25c6
jliao <joyceliao2001@gmail.com>
2022-07-29 12:03:49 -0700

    Utilized pickr online custom color palette and created shades

--- javascript.js
+++ javascript.js
@@ -30,30 +30,29 @@ cells.forEach(cell => {
   //    cell.style.backgroundColor = "white";
   //  });
  })
- 
-//bw button
+
+
 
 const bw = document.getElementById("bw");
 function blackWhite() {
   bw.addEventListener("click", ()=>{
-    cells.forEach(cell =>
+    cells.forEach(cell => {
       cell.addEventListener("mouseenter", () => {
-        cell.style.backgroundColor ="black"
-      }))
+        const shades = [
+          "grey",
+          "dimgray",
+          "whitesmoke",
+          "lightgray",
+          "silver",
+          "black",
+          "gainsboro"
+        ]
+       randomShade = Math.floor(Math.random()*shades.length);
+        cell.style.backgroundColor =`${shades[randomShade]}`;
+    });
+      })
   })
 }
-
-//on user selection of custom
-const custom = document.getElementById("custom");
-function colorSelected() {
-  custom.addEventListener("click", (element) =>{
-    cells.forEach(cell =>{
-      cell.addEventListener("mouseenter", () =>{
-        cell.style.backgroundColor = `#${element.value}`;
-      });
-    });
-  })
-};
 
 const eraser = document.getElementById("eraser");
 function erase() {
@@ -81,6 +80,56 @@ function random(){
   });
 };
 
+
+const pickr = Pickr.create({
+  el: '.color-picker',
+  theme: 'classic', 
+  
+  swatches: [
+      'rgba(244, 67, 54, 1)',
+      'rgba(233, 30, 99, 0.95)',
+      'rgba(156, 39, 176, 0.9)',
+      'rgba(103, 58, 183, 0.85)',
+      'rgba(63, 81, 181, 0.8)',
+      'rgba(33, 150, 243, 0.75)',
+      'rgba(3, 169, 244, 0.7)',
+      'rgba(0, 188, 212, 0.7)',
+      'rgba(0, 150, 136, 0.75)',
+      'rgba(76, 175, 80, 0.8)',
+      'rgba(139, 195, 74, 0.85)',
+      'rgba(205, 220, 57, 0.9)',
+      'rgba(255, 235, 59, 0.95)',
+      'rgba(255, 193, 7, 1)'
+  ],
+
+  components: {
+
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+          hex: true,
+          rgba: true,
+          hsla: true,
+          hsva: true,
+          cmyk: true,
+          input: true,
+      }
+  }
+});
+
+pickr.on("change", (color, instance) =>{
+  const customColor = color.toRGBA().toString();
+  cells.forEach(cell => {
+    cell.addEventListener("mouseenter", () => {
+      cell.style.backgroundColor =customColor;
+    })
+  })
+});
+
 random();
-colorSelected();
+blackWhite();
 erase();
